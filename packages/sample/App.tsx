d577f79d4e07d661e44de7f31dfc1a4e6522e173
Grant Sander <gksander93@gmail.com>
2022-06-10 11:29:09 -0500

    Update App.tsx

--- packages/sample/App.tsx
+++ packages/sample/App.tsx
@@ -1,4 +1,5 @@
-import { SafeAreaView, Text, View } from "react-native";
+import * as React from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import {
   createStyleBuilder,
   defaultHandlers,
@@ -10,32 +11,45 @@ const { makeStyledComponent } = createStyleBuilder({
 });
 
 const StyledView = makeStyledComponent(View);
+const StyledTouchableOpacity = makeStyledComponent(TouchableOpacity);
 const StyledText = makeStyledComponent(Text);
 const StyledSafeAreaView = makeStyledComponent(SafeAreaView);
 
 export default function App() {
+  const [colorScheme, setColorScheme] = React.useState(
+    "light" as "light" | "dark"
+  );
+
+  console.log(colorScheme);
+
   return (
-    <StyleProvider colorScheme="auto">
+    <StyleProvider colorScheme={colorScheme}>
       <StyledSafeAreaView
-        bg="red-300"
+        bg="red-200"
         bg__dark="red-800"
         flex="1"
         items="center"
         justify="center"
         p="6"
       >
-        <StyledView
+        <StyledTouchableOpacity
           p="6"
-          bg="blue-400"
+          bg="pink-200"
+          bg__dark="pink-800"
           bg-opacity="80"
           rounded="lg"
           border="1"
           border-color="red-500"
-          shadow="xl"
+          shadow="2xl"
           accessibilityHint="Just to show you the rest of props are here"
+          onPress={() => {
+            setColorScheme((v) => (v === "light" ? "dark" : "light"));
+          }}
         >
-          <StyledText>Open up App.tsx to start working on your app!</StyledText>
-        </StyledView>
+          <StyledText color="gray-800" color__dark="gray-300">
+            Open up App.tsx to start working on your app!
+          </StyledText>
+        </StyledTouchableOpacity>
       </StyledSafeAreaView>
     </StyleProvider>
   );
