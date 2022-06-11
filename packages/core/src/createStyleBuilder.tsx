aaf330d0be98b3253bfd3512d3609d1c6f046d55
Grant Sander <gksander93@gmail.com>
2022-06-11 07:28:04 -0500

    Small tweaks to wrappedComponent

--- packages/core/src/createStyleBuilder.tsx
+++ packages/core/src/createStyleBuilder.tsx
@@ -7,6 +7,9 @@ import { colorStringToRgb } from "./utils/colorStringToRgb";
 const darkReg = /^(.*)__dark$/;
 const isDark = <T extends string>(name: T) => darkReg.test(name);
 
+/**
+ * Core builder fn. Takes in a set of handlers, and gives back a hook and component-builder.
+ */
 export const createStyleBuilder = <StyleHandlers extends StyleHandlerSet>({
   handlers: _handlers,
 }: {
@@ -22,6 +25,9 @@ export const createStyleBuilder = <StyleHandlers extends StyleHandlerSet>({
     handlers[`${key}__dark`] = handlers[key];
   }
 
+  /**
+   * Core hook to apply styles based on props/style object
+   */
   const useStyle = <P extends StyleProps<StyleHandlers>>(styleProps: P) => {
     const { isDarkMode } = React.useContext(StyleContext);
 
@@ -29,6 +35,7 @@ export const createStyleBuilder = <StyleHandlers extends StyleHandlerSet>({
       const baseStyles = {};
       const darkStyles = {};
 
+      // Build out base/dark styles using handler fn's
       for (const key in styleProps) {
         if (key in handlers) {
           Object.assign(
@@ -38,6 +45,7 @@ export const createStyleBuilder = <StyleHandlers extends StyleHandlerSet>({
         }
       }
 
+      // Our end-result styles are base + (dark if necessary)
       const styles = Object.assign(
         baseStyles,
         isDarkMode ? darkStyles : {}
@@ -57,23 +65,43 @@ export const createStyleBuilder = <StyleHandlers extends StyleHandlerSet>({
     }, [styleProps]);
   };
 
-  // Utility to make a styled component
+  /**
+   * Utility to make a styled component
+   */
+  // TODO: forwardRef, and maybe try to memoize
   const makeStyledComponent = <T,>(
     WrappedComponent: JSXElementConstructor<T>
   ) => {
-    const ComponentWithStyles = <P extends StyleProps<StyleHandlers>>({
-      style,
-      ...rest
-    }: Omit<T, keyof P> & P) => {
+    const ComponentWithStyles = <P extends StyleProps<StyleHandlers>>(
+      props: Omit<T, keyof P> & P
+    ) => {
+      const ref = React.useRef(null);
+      const styleProps = {};
+      const componentProps = {};
+
+      for (const key in props) {
+        if (key in handlers) {
+          // @ts-ignore
+          styleProps[key] = props[key];
+        } else {
+          // @ts-ignore
+          componentProps[key] = props[key];
+        }
+      }
+
       // TODO: Filter out non-applicable props?
       // @ts-ignore
-      const addedStyles = useStyle(rest);
+      const addedStyles = useStyle(styleProps);
+
+      // @ts-ignore
+      ref.current?.setNativeProps?.(addedStyles);
 
       return (
         // @ts-ignore
         <WrappedComponent
-          {...rest}
-          style={[addedStyles, ...[Array.isArray(style) ? style : [style]]]}
+          ref={ref}
+          {...componentProps}
+          // style={[addedStyles, ...[Array.isArray(style) ? style : [style]]]}
         />
       );
     };
--- packages/sample/App.tsx
+++ packages/sample/App.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Animated } from "react-native";
 import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import {
   createStyleBuilder,
@@ -11,11 +12,24 @@ const { makeStyledComponent } = createStyleBuilder({
 });
 
 const StyledView = makeStyledComponent(View);
-const StyledTouchableOpacity = makeStyledComponent(TouchableOpacity);
+const StyledTouchableOpacity = makeStyledComponent(
+  Animated.createAnimatedComponent(TouchableOpacity)
+);
 const StyledText = makeStyledComponent(Text);
 const StyledSafeAreaView = makeStyledComponent(SafeAreaView);
 
 export default function App() {
+  const x = React.useRef(new Animated.Value(0)).current;
+
+  const startAnim = React.useCallback(() => {
+    x.setValue(0);
+    Animated.timing(x, {
+      toValue: 300,
+      duration: 3000,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   const [colorScheme, setColorScheme] = React.useState(
     "light" as "light" | "dark"
   );
@@ -40,9 +54,8 @@ export default function App() {
           border-color="red-500"
           shadow="2xl"
           accessibilityHint="Just to show you the rest of props are here"
-          onPress={() => {
-            setColorScheme((v) => (v === "light" ? "dark" : "light"));
-          }}
+          onPress={startAnim}
+          style={{ transform: [{ translateY: x }] }}
         >
           <StyledText color="gray-800" color__dark="gray-300">
             Open up App.tsx to start working on your app!
