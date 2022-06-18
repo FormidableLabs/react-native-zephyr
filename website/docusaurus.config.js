10d763a6dfb2d07bad4349614c302d69a081ff6c
Grant Sander <gksander93@gmail.com>
2022-06-17 19:22:07 -0500

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "React Native Zephyr",
   tagline: "React Native styling library",
-  url: "https://react-native-zephyr.vercel.app/",
-  baseUrl: "/",
+  url: "https://formidable.com",
+  baseUrl: "/open-source/react-native-zephyr/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
