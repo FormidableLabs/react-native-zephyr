8af64d584551acd12ad52482dae4ca6c4c687611
Grant Sander <gksander93@gmail.com>
2022-06-23 21:52:23 -0500

    HSL opacity

--- packages/core/src/utils/applyOpacityToColor.ts
+++ packages/core/src/utils/applyOpacityToColor.ts
@@ -4,6 +4,9 @@ export const applyOpacityToColor = (color: string, opacity: number) => {
   if (rgbReg.test(trimmedColor))
     return applyOpacityToRgb(trimmedColor, opacity);
 
+  if (hslReg.test(trimmedColor))
+    return applyOpacityToHsl(trimmedColor, opacity);
+
   return trimmedColor;
 };
 
@@ -18,5 +21,26 @@ const applyOpacityToRgb = (rgbString: string, opacity: number) => {
   }
 };
 
+const applyOpacityToHsl = (hslString: string, opacity: number) => {
+  const match = hslString.match(hslReg);
+  if (match?.groups) {
+    const { h, s, l, a } = match.groups;
+    if (a) return `hsla(${h},${s},${l},${+a * opacity})`;
+    return `hsla(${h},${s},${l},${opacity})`;
+  } else {
+    return hslString;
+  }
+};
+
+const applyOpacityToHex = (hexString: string, opacity: number) => {};
+
+const decimalToHex = (decimal: number) => {
+  const clampedDecimal = Math.max(0, Math.min(1, decimal));
+  const intValue = Math.round(clampedDecimal * 255);
+  const hexValue = intValue.toString(16);
+  return hexValue.padStart(2, "0");
+};
+
 const colorStrip = / /g;
 const rgbReg = /^rgba?\((?<r>\d+),(?<g>\d+),(?<b>\d+),?(?<a>0?\.?\d+)?\)$/;
+const hslReg = /^hsla?\((?<h>\d+),(?<s>\d+%?),(?<l>\d+%?),?(?<a>0?\.?\d+)?\)$/;
